abe0d50f2410dc56dd6d1f35a7e22c4e839a614d
Matthew (AE-194 near the Door) <mweissman@horizon.csueastbay.edu>
2019-09-03 20:17:21 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 function setup() {
   createCanvas(600,600);
-
+  noStroke();
 }
 
 function draw() {
-  fillMode(HSL);
+  colorMode(HSB);
   fill(8,100,100);
-  ellipse(50,50,50,50);
+  ellipse(200,200,50,50);
+  fill(8,0,100);
+  ellipse(200,200,33,33);
+  fill(8,100,100);
+  ellipse(200,200,16,16);
 }
